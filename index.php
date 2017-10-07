f910ad10fc31c601e5a24995f68c2413ec266494
Thennyson Passos <thennyson@gmail.com>
2017-10-07 09:15:55 -0500

    - Movendo código do slider para Módulo; - Index com código mais limpo; - Arquivos inúteis deletados.

--- index.php
+++ index.php
@@ -1,81 +1,12 @@
 <?php get_header(); ?>
 
 <div class="container" style="padding-top: 64px;">
-<!-- INICIO - Módulo - Chips -->
-<!--
-  <div class="row"  style="padding: 0 10px;">
-     <?php //include(TEMPLATEPATH.'/menu-integracao.php');?>
-  </div> -->
-<!-- FIM - Módulo - Chips -->
-<!-- INICIO - Módulo - Slider -->
-  <div class="row">
-   <div class="slider-noticias-container">
 
-    <div class="col s12 m12 l6 no-padding">
-    <?php query_posts('showposts=1&tag=slider');?>
-    <?php if (have_posts()): while (have_posts()) : the_post();?>
-	<a href="<?php the_Permalink()?>" title="<?php the_title();?>" >
-	<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'slider-noticias2' ); ?>
-      <div class="slider-noticias img-slider" style="background: url('<?php echo $image[0]; ?>');">
-        <div class="bloco-slider-noticias">
-          <h2><?php the_title();?></h2>
-        </div>
-      </div>
-	</a>
-<?php endwhile;endif;?>
-<div class="clearfix"></div>
-    </div>
+<!-- Inicio - Módulo Slider -->
+<?php include(TEMPLATEPATH.'/mod-slider.php'); ?>
+<!-- Fim - Módulo Slider -->
 
-    <div class="col s12 m12 l6 no-padding">
-    <?php query_posts('showposts=1&tag=slider&offset=1');?>
-    <?php if (have_posts()): while (have_posts()) : the_post();?>
-	<a href="<?php the_Permalink()?>" title="<?php the_title();?>" >
-	<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'slider-noticias2' ); ?>
-      <div class="slider-noticias img-slider" style="background: url('<?php echo $image[0]; ?>');">
-        <div class="bloco-slider-noticias">
-          <h2></span><?php the_title();?></h2>
-        </div>
-      </div>
-	</a>
-<?php endwhile;endif;?>
-<div class="clearfix"></div>
-    </div>
-
-    <div class="col s12 m12 l6 no-padding">
-    <?php query_posts('showposts=1&tag=slider&offset=2');?>
-    <?php if (have_posts()): while (have_posts()) : the_post();?>
-	<a href="<?php the_Permalink()?>" title="<?php the_title();?>" >
-	<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'slider-noticias2' ); ?>
-      <div class="slider-noticias img-slider" style="background: url('<?php echo $image[0]; ?>');">
-        <div class="bloco-slider-noticias">
-          <h2></span><?php the_title();?></h2>
-        </div>
-      </div>
-	</a>
-<?php endwhile;endif;?>
-<div class="clearfix"></div>
-    </div>
-
-    <div class="col s12 m12 l6 no-padding">
-    <?php query_posts('showposts=1&tag=slider&offset=3');?>
-    <?php if (have_posts()): while (have_posts()) : the_post();?>
-	<a href="<?php the_Permalink()?>" title="<?php the_title();?>" >
-	<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'slider-noticias2' ); ?>
-      <div class="slider-noticias img-slider" style="background: url('<?php echo $image[0]; ?>');">
-        <div class="bloco-slider-noticias">
-          <h2></span><?php the_title();?></h2>
-        </div>
-      </div>
-	</a>
-<?php endwhile;endif;?>
-<div class="clearfix"></div>
-    </div>
-
-  </div>
-  </div>
-  </div>
-  <!-- FIM - Módulo - Slider -->
-
+</div>
 <div id="especiais" class="container">
 <?php /* include(TEMPLATEPATH.'/mod-radios.php'); */ ?>
 </div>
---
+++ mod-slider.php
@@ -0,0 +1,72 @@
+<div class="row">
+   <div class="slider-noticias-container2">
+
+    <div class="col s6 m6 l6 no-padding">
+
+    <div class="slide-principal">
+    <?php query_posts('showposts=1&tag=slider');?>
+    <?php if (have_posts()): while (have_posts()) : the_post();?>
+	<a href="<?php the_Permalink()?>" title="<?php the_title();?>" >
+	<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'slider-noticias2' ); ?>
+      <div class="slider-noticias img-slider" style="background: url('<?php echo $image[0]; ?>');">
+        <div class="bloco-slider-noticias">
+          <h2><?php the_title();?></h2>
+        </div>
+      </div>
+	</a>
+<?php endwhile;endif;?>
+<div class="clearfix"></div>
+    </div>
+
+    <div class="slide-secundario">
+    <?php query_posts('showposts=1&tag=slider&offset=1');?>
+    <?php if (have_posts()): while (have_posts()) : the_post();?>
+	<a href="<?php the_Permalink()?>" title="<?php the_title();?>" >
+	<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'slider-noticias2' ); ?>
+      <div class="slider-noticias img-slider" style="background: url('<?php echo $image[0]; ?>');">
+        <div class="bloco-slider-noticias">
+          <h2><?php the_title();?></h2>
+        </div>
+      </div>
+	</a>
+<?php endwhile;endif;?>
+<div class="clearfix"></div>
+    </div>
+
+    </div>
+    <div class="col s6 m6 l6 no-padding">
+
+    <div class="slide-principal">
+    <?php query_posts('showposts=1&tag=slider&offset=2');?>
+    <?php if (have_posts()): while (have_posts()) : the_post();?>
+  <a href="<?php the_Permalink()?>" title="<?php the_title();?>" >
+  <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'slider-noticias2' ); ?>
+      <div class="slider-noticias img-slider" style="background: url('<?php echo $image[0]; ?>');">
+        <div class="bloco-slider-noticias">
+          <h2><?php the_title();?></h2>
+        </div>
+      </div>
+  </a>
+<?php endwhile;endif;?>
+<div class="clearfix"></div>
+    </div>
+
+    <div class="slide-secundario">
+    <?php query_posts('showposts=1&tag=slider&offset=3');?>
+    <?php if (have_posts()): while (have_posts()) : the_post();?>
+  <a href="<?php the_Permalink()?>" title="<?php the_title();?>" >
+  <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'slider-noticias2' ); ?>
+      <div class="slider-noticias img-slider" style="background: url('<?php echo $image[0]; ?>');">
+        <div class="bloco-slider-noticias">
+          <h2><?php the_title();?></h2>
+        </div>
+      </div>
+  </a>
+<?php endwhile;endif;?>
+<div class="clearfix"></div>
+    </div>
+
+    </div>
+
+  </div>
+  </div>
