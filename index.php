ab917accc5860d6323e4f93f27b44345d2174d2d
Thennyson Passos <thennyson@gmail.com>
2017-10-07 10:46:13 -0500

    - Criando módulo Destaques; - Movendo todos os módutos para a pasta Mods.

--- index.php
+++ index.php
@@ -3,8 +3,12 @@
 <div class="container" style="padding-top: 64px;">
 
 <!-- Inicio - Módulo Slider -->
-<?php include(TEMPLATEPATH.'/mod-slider.php'); ?>
+<?php include(TEMPLATEPATH.'/mods/mod-slider.php'); ?>
 <!-- Fim - Módulo Slider -->
+
+<!-- Inicio - Módulo Destaques -->
+<?php include(TEMPLATEPATH.'/mods/mod-destaques.php'); ?>
+<!-- Fim - Módulo Destaques -->
 
 </div>
 <div id="especiais" class="container">
---
+++ mods/mod-destaques.php
@@ -0,0 +1,51 @@
+<div class="row">
+
+  <div class="col s12 m12 l4">
+    <?php query_posts('showposts=1&tag=destaques');?>
+    <?php if (have_posts()): while (have_posts()) : the_post();?>
+      <div class="img-destaque">
+        <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'slider-noticias2' ); ?>
+        <a href="<?php the_Permalink()?>" title="<?php the_title();?>" >
+          <img src="<?php echo $image[0]; ?>">
+        </a>
+      </div>
+      <a href="<?php the_Permalink()?>" title="<?php the_title();?>" >
+        <h2><?php the_title();?></h2>
+      </a>
+    <?php endwhile;endif;?>
+    <div class="clearfix"></div>
+  </div>
+
+  <div class="col s12 m12 l4">
+    <?php query_posts('showposts=1&tag=destaques&offset=1');?>
+    <?php if (have_posts()): while (have_posts()) : the_post();?>
+      <div class="img-destaque">
+        <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'slider-noticias2' ); ?>
+        <a href="<?php the_Permalink()?>" title="<?php the_title();?>" >
+          <img src="<?php echo $image[0]; ?>">
+        </a>
+      </div>
+      <a href="<?php the_Permalink()?>" title="<?php the_title();?>" >
+        <h2><?php the_title();?></h2>
+      </a>
+    <?php endwhile;endif;?>
+    <div class="clearfix"></div>
+  </div>
+
+  <div class="col s12 m12 l4">
+    <?php query_posts('showposts=1&tag=destaques&offset=2');?>
+    <?php if (have_posts()): while (have_posts()) : the_post();?>
+      <div class="img-destaque">
+        <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'slider-noticias2' ); ?>
+        <a href="<?php the_Permalink()?>" title="<?php the_title();?>" >
+          <img src="<?php echo $image[0]; ?>">
+        </a>
+      </div>
+      <a href="<?php the_Permalink()?>" title="<?php the_title();?>" >
+        <h2><?php the_title();?></h2>
+      </a>
+    <?php endwhile;endif;?>
+    <div class="clearfix"></div>
+  </div>
+
+</div>
