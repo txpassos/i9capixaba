176336ee97603ea05ac2bc0d8c2583e825b6d01e
Thennyson Passos <thennyson@gmail.com>
2017-10-09 07:11:55 -0500

    Instalando código do Google Analytics

--- header.php
+++ header.php
@@ -14,6 +14,16 @@
         <!-- CSS  -->
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <!-- Global Site Tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-107789034-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-107789034-1');
+        </script>
+
     </head>
     <body>
       <!-- INICIO - Módulo - Navegação -->
